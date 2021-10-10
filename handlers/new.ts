a10bfe1173cf7c2dea43571db877a7ab74f860bc
Siddharth <siddharth.muscat@gmail.com>
2021-10-10 14:42:48 +0530

    Create new handler

--- handlers/new.ts
+++ handlers/new.ts
@@ -1,1 +1,20 @@
-export async function newHandler(argv: Args) {}
+import {Args, ensureDirSync, existsSync} from '../deps.ts';
+import {fail, prompt} from '../ui.ts';
+
+export async function newHandler(argv: Args) {
+	const name = argv.name ?? argv._[1] ?? await prompt('What is the name of the project');
+
+	if (existsSync(name.replace(' ', '-'))) {
+		fail('File/Folder already exists');
+	}
+
+	const nameSlug = name.replace(/\s|\/|\\/g, '-');
+
+	ensureDirSync(`${nameSlug}/src/components`);
+	Deno.writeTextFileSync(`${nameSlug}/src/App.sleek`, 'Todo');
+	Deno.writeTextFileSync(`${nameSlug}/config.json`, JSON.stringify({
+		entry: 'App.sleek'
+	}, null, '	'));
+	console.log('Initialized project', name);
+}
+
