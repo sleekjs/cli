c380c1d1ce0dabf046352a7040524a54f11d2f5f
Siddharth <siddharth.muscat@gmail.com>
2021-10-10 21:30:03 +0530

    Create build command

--- handlers/build.ts
+++ handlers/build.ts
@@ -1,3 +1,29 @@
-import {Args} from '../deps.ts';
+import {Args, parse, existsSync, ensureDirSync} from '../deps.ts';
+import {fail} from '../ui.ts';
 
-export function buildHandler(argv: Args) {}
+export function buildHandler(argv: Args) {
+	if (!existsSync('config.json')) {
+		fail('Not a sleek app!');
+	}
+
+	const config = JSON.parse(Deno.readTextFileSync('config.json'));
+
+	ensureDirSync('dist');
+	Deno.chdir('src');
+
+	for (const path in config.entries || {}) {
+		const entry = config.entries[path];
+		let {HTML, CSS, JS} = parse(Deno.readTextFileSync(entry)); // TODO HTML template customizable
+
+		HTML = HTML.replace(/<%css|js%>/g, 'bundle');
+
+		ensureDirSync(`../dist/${path}`)
+		Deno.writeTextFileSync(`../dist/${path}/index.html`, HTML);
+		Deno.writeTextFileSync(`../dist/${path}/bundle.css`, CSS);
+		Deno.writeTextFileSync(`../dist/${path}/bundle.js`, JS);
+
+		console.log('Built', entry);
+
+		// TODO url params etc.
+	}
+}
