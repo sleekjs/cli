b485db6b0d5206717edb147b7d51bbeecf929417
Siddharth <siddharth.muscat@gmail.com>
2021-10-10 14:41:38 +0530

    Add deps

---
+++ deps.ts
@@ -0,0 +1,8 @@
+export {parse} from 'https://deno.land/std@0.110.0/flags/mod.ts';
+export type {Args, ArgParsingOptions} from 'https://deno.land/std@0.106.0/flags/mod.ts'
+
+export {ensureDirSync, existsSync} from 'https://deno.land/std@0.110.0/fs/mod.ts';
+
+export const CLI_VERSION = '1.0.0';
+export const ENGINE_VERSION = '1.0.0';
+
