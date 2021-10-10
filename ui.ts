7ea828bca88c33ba43a2adabe3cff928457e2431
Siddharth <siddharth.muscat@gmail.com>
2021-10-10 14:42:26 +0530

    Add ui handlers

---
+++ ui.ts
@@ -0,0 +1,17 @@
+/*
+ * Prompt for a response
+ */
+export async function prompt(message: string = "") {
+	const buf = new Uint8Array(1024);
+	await Deno.stdout.write(new TextEncoder().encode(`\u001b[33m?\u001b[36m ${message}?\u001b[0m `));
+	const n = <number>await Deno.stdin.read(buf);
+	return new TextDecoder().decode(buf.subarray(0, n)).trim();
+}
+
+/*
+ * Fail with error
+ */
+export const fail = (why: string = 'Unkown error') => {
+	console.error('Error:', why);
+	Deno.exit(1);
+}
