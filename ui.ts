fb4c5fa31523742a9d4495f600b4ac9941ee4ad8
Siddharth <siddharth.muscat@gmail.com>
2021-10-11 12:02:34 +0530

    Make ui utilities allow any number of arguments

--- ui.ts
+++ ui.ts
@@ -1,7 +1,7 @@
 /*
  * Prompt for a response
  */
-export async function prompt(message: string = "") {
+export async function prompt(message = '') {
 	const buf = new Uint8Array(1024);
 	await Deno.stdout.write(new TextEncoder().encode(`\u001b[33m?\u001b[36m ${message}?\u001b[0m `));
 	const n = <number>await Deno.stdin.read(buf);
@@ -11,7 +11,7 @@ export async function prompt(message: string = "") {
 /*
  * Fail with error
  */
-export const fail = (why: string = 'Unkown error') => {
-	console.error('Error:', why);
+export const fail = (...why: unknown[])=> {
+	console.error('Error:', ...why);
 	Deno.exit(1);
 }
