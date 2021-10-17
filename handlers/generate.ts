7b7409e00ba737f23b6c65b24dcc354d42c49131
Siddharth <siddharth.muscat@gmail.com>
2021-10-17 18:47:13 +0530

    Write in src folder

--- handlers/generate.ts
+++ handlers/generate.ts
@@ -1,4 +1,4 @@
-import {Args, basename, dirname} from '../deps.ts';
+import {Args, basename, dirname, ensureDirSync} from '../deps.ts';
 import {fail} from '../ui.ts';
 import {failWhenNotProject} from '../utils/failWhenNotProject.ts';
 
@@ -20,15 +20,19 @@ type Schema = (name: string, path: string) => void;
 
 const schemas: Record<string, Schema> = {
 	component: (name: string, path: string) => {
-		Deno.writeTextFile(`${path}/${name}.sleek`, `<p>${name} works!</p>`);
+		ensureDirSync(path);
+		Deno.writeTextFile(`src/${path}/${name}.sleek`, `<p>${name} works!</p>`);
+		console.log('Created component');
 	},
 	service: (name: string, path: string) => {
+		ensureDirSync(path);
 		Deno.writeTextFile(
-			`${path}/${name}.js`,
+			`src/${path}/${name}.js`,
 			`export const ${name} = () => console.log('${name} works!');`
 		);
+		console.log('Created service');
 	},
+};
 
-	c: (this as unknown as Record<string, Schema>).component as Schema,
-	s: (this as unknown as Record<string, Schema>).service as Schema,
-}
+schemas.c = schemas.component;
+schemas.s = schemas.service;
